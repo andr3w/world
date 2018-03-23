199b703e7377d6f33992b221dacb978993bc464f
Andrew Cumming <a.cumming@napier.ac.uk>
2018-03-23 00:38:44 +0000

    Removed pretty much everything. Don't want to spoil the fun.

--- server.py
+++ server.py
@@ -1,28 +1,9 @@
-from flask import Flask, render_template
-import json
-w = json.load(open('worldl.json'))
+from flask import Flask
 app = Flask(__name__)
-
-@app.route('/country/<num>')
-def countryPage(num):
-  return render_template('country.html',c = w[int(num)])
-
-@app.route('/countryByName/<name>')
-def countryByNamePage(name):
-  return render_template('country.html',
-          c = next(c for c in w if c['name'] == name)
-          )
-
-@app.route('/continent/<name>')
-def continentPage(name):
-  return render_template('continent.html',
-          continentName = name,
-          countryList = [c for c in w if c['continent']==name]
-          )
 
 @app.route('/')
 def indexPage():
-  return render_template('index.html',continentList = set([c['continent'] for c in w]))
+  return "{}".format(6*7)
 
 if __name__=='__main__':
   app.run(host='0.0.0.0',port=5036,debug=True)
